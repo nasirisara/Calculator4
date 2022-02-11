2bcf594eebe03ca9b032b740d4ffe6de1ccbcda1
nasirisara <99367926+nasirisara@users.noreply.github.com>
2022-02-11 09:37:40 +0100

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please insert num1");
+            Console.WriteLine("please insert +, -, *,/ as operators!");
+            Console.WriteLine("please insert num2");
+
+            string strnum1, opera, strnum2;
+            strnum1 = Console.ReadLine();
+            opera = Console.ReadLine();
+            strnum2 = Console.ReadLine();
+            double num1, num2;
+            num1 = double.Parse(strnum1);
+            num2 = double.Parse(strnum2);
+
+            switch (opera)
+            {
+                case "+":
+                    Console.WriteLine(num1 + num2);
+                    break;
+
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+
+                case "/":
+                    Console.WriteLine(num1 / num2);
+                    break;
+
+                case "*":
+                    Console.WriteLine(num1 * num2);
+                    break;
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
